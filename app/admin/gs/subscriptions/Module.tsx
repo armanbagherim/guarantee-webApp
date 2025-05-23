1aff6f6fcd47b3d31c99df43e6722767ebea1921
Arman <user@MacBookPro.lan>
2025-05-23 23:35:03 +0330

    add subscriptions

---
+++ app/admin/gs/subscriptions/Module.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { pageTitle } from "../../layout";
+import { useAtom } from "jotai";
+import LightDataGrid from "@/app/components/admin-components/LightDataGrid/LightDataGrid";
+import { columns } from "./columns";
+import DataHandler from "./DataHandler";
+import { fetcher, useFetcher } from "@/app/components/admin-components/fetcher";
+import { useFormik } from "formik";
+import { ConvertToNull } from "@/app/components/utils/ConvertToNull";
+import toast from "react-hot-toast";
+import { Button } from "@mui/material";
+
+export default function EavTypesModule() {
+  const [title, setTitle] = useAtom(pageTitle);
+  const [triggered, setTriggered] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const [isEditEav, setIsEditEav] = useState({
+    open: false,
+    id: null,
+    active: false,
+  });
+
+  useEffect(() => {
+    setTitle({
+      title: "شماره ها",
+      buttonTitle: null,
+      link: null,
+      onClick: null,
+    });
+  }, []);
+
+  const downloadFile = async () => {
+    try {
+      // Fetch the file as a blob
+      const response = await fetcher({
+        url: `/v1/api/guarantee/admin/subscriptions/excel`,
+        method: "GET",
+        responseType: 'blob', // Important for file downloads
+      });
+
+      // Create a download link
+      const url = window.URL.createObjectURL(new Blob([response]));
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', `numbers.xlsx`);
+      document.body.appendChild(link);
+      link.click();
+
+      // Clean up
+      link.parentNode?.removeChild(link);
+      window.URL.revokeObjectURL(url);
+
+    } catch (err) {
+      toast.error(err.message);
+    }
+  };
+
+  return (
+    <div>
+      <div className="mb-4">
+        <Button onClick={downloadFile} fullWidth variant="contained" color="primary">دریافت خروجی اکسل</Button>
+      </div>
+
+
+      <LightDataGrid
+        triggered={triggered}
+        url={"/v1/api/guarantee/admin/subscriptions"}
+        columns={columns(
+
+        )}
+      />
+    </div>
+  );
+}
---
+++ app/admin/gs/subscriptions/columns.tsx
@@ -0,0 +1,45 @@
+import { Tooltip } from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+import Link from "next/link";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import Uploader from "@/app/components/admin-components/Uploader";
+import { fetcher } from "@/app/components/admin-components/fetcher";
+import Loading from "@/app/components/admin-components/loading";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import React, { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import Image from "next/image";
+import { Button, IconButton } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import ModeEditIcon from "@mui/icons-material/ModeEdit";
+import Swal from "sweetalert2";
+import SubjectIcon from "@mui/icons-material/Subject";
+import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
+
+export function columns(
+  isEditEav,
+  setIsEditEav,
+  triggered,
+  setTriggered,
+  formik
+) {
+  const getData = async (id: string) => {
+    try {
+      const res = await fetcher({
+        url: `/v1/api/guarantee/admin/variants/${id}`,
+        method: "GET",
+      });
+      return res.result;
+    } catch (err) {
+      toast.error(err.message);
+    }
+  };
+
+  return [
+    {
+      accessorKey: "phoneNumber",
+      header: "شماره موبایل ",
+    },
+
+  ];
+}
